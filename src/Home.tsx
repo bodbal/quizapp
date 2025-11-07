679717f96d049693559d80c433efa26b952c62a6
bodbal <b1bodbal@vasvari.org>
2025-11-07 10:12:09 +0100

    react wip

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function Home() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <div>
+        
+     </div>
     </>
   )
 }
